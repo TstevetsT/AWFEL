da57f704f3837b974c9bfb07313099a057470f47
TstevetsT <TstevetsT@users.noreply.github.com>
2023-10-10 23:27:27 -0700

    First Commit of starter code

---
+++ awfel.py
@@ -0,0 +1,7 @@
+# Apple Workout Fitness Export Linker (AWFEL) -python script that takes pictures of your swim workout splits as an input, uses OCR to convert to text, exports to excel, and provides analysis on best swim intervals.
+# This is a work in progress.  Have ideas how to make it work thanks to skills aquired in VetsInTech Python course.  Thank You to Brion!
+# Initial plan
+# -Use tesseract to extract text from photos
+# -Use string/text to format the data
+# -Use Openpyxl to export data to worksheet for analysis
+# -Add any other features that I come up with.
